f7d35242301b4a07980fb5f8fa6c2c4db7fcab17
Esa Varemo <esa@kuivanto.fi>
2013-10-18 23:12:59 +0300

    Implemented prettier printing

--- twt_client/views.py
+++ twt_client/views.py
@@ -3,6 +3,7 @@ import datetime
 
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.utils import timezone
 
 from twt_client.models import Tweet, Meta
 from twt_client.twitter_api import list_timeline
@@ -17,16 +18,16 @@ def hello(request):
     html = "<html><body><h1>Hello</h1></body></html>"
     return HttpResponse(html)
 
-def update(request):
+def update(request=None):
     tweets = list_timeline()
 
     for tweet in tweets:
         Tweet(id = tweet['id'], data = json.dumps(tweet)).save()
 
-    return HttpResponse("<html><head></head><body>success</body></html>")
+    if request is not None:
+        return HttpResponse("<html><head></head><body>success</body></html>")
 
-def list_tweets(request):
-
+def update_if_necessary():
     try:
         expiretime = int(Meta.objects.get(id=0).time) + 300
     except Meta.DoesNotExist:
@@ -39,7 +40,7 @@ def list_tweets(request):
 
     if now > expiretime:
         print("Not cached")
-        update(request)
+        update()
 
         newtime = Meta.objects.get(id=0)
         newtime.time = datetime.datetime.now().timestamp()
@@ -47,7 +48,25 @@ def list_tweets(request):
     else:
         print("Cached")
 
+def list_tweets(request):
+    update_if_necessary()
+
     tweets = []
     for tweet in sorted(Tweet.objects.all(), key=lambda tweet: datetime.datetime.strptime(json.loads(tweet.data)['created_at'], "%a %b %d %H:%M:%S %z %Y").timestamp(), reverse=False):
-        tweets.append(json.loads(tweet.data))
+        data = json.loads(tweet.data)
+
+        time = datetime.datetime.strptime(json.loads(tweet.data)['created_at'], "%a %b %d %H:%M:%S %z %Y").timestamp()
+        if time > timezone.now().timestamp() - 60*60*24:
+            if time > timezone.now().timestamp() - 60*60:
+                minutes  = (timezone.now() - datetime.datetime.strptime(json.loads(tweet.data)['created_at'], "%a %b %d %H:%M:%S %z %Y")).seconds/60
+                pretty = str(round(minutes)) + "m ago"
+            else:
+                hours  = (timezone.now() - datetime.datetime.strptime(json.loads(tweet.data)['created_at'], "%a %b %d %H:%M:%S %z %Y")).seconds/3600
+                pretty = str(round(hours))+ "h ago"
+        else:
+            pretty = datetime.datetime.strptime(json.loads(tweet.data)['created_at'], "%a %b %d %H:%M:%S %z %Y").strftime("%d.%m %H:%M")
+
+        data['pretty_time'] = pretty
+        tweets.append(data)
+
     return render_to_response('tweets.html', {'tweets':tweets})
