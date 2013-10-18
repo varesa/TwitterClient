d97c81b08e85aa1b192f1cb7ee86d99f08dc3a0c
Esa Varemo <esa@kuivanto.fi>
2013-10-18 19:52:03 +0300

    Create a database model for tweets

--- twt_client/models.py
+++ twt_client/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Tweet(models.Model):
+    id = models.IntegerField(primary_key=True)
+    data = models.CharField(max_length=16384)
